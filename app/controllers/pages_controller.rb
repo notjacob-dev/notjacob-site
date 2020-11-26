aadc69fb822db69470f10400afef63dd125231c5
Jacob <jacobblair6789@gmail.com>
2020-11-26 01:33:42 -0700

    Add /about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,17 @@
 class PagesController < ApplicationController
   def home
   end
+
   def contact
   end
+
   def handle_form
     puts "MESSAGE => " + request.params["name"] + " <" + request.params["email"] + ">: " + request.params["message"]
+  end
+
+  def handle_form_get
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get "/contact", to: "pages#contact"
+
   post "/handle-form", to: "pages#handle_form"
+  get "/handle-form", to: "pages#handle_form_get"
+
+  get "/about", to: "pages#about"
 end
