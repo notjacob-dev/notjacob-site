bb2df18ff39ef102239e12d20f2c81358b0b37b2
Jacob <jacobblair6789@gmail.com>
2020-11-25 18:55:57 -0700

    now there are multiple webpages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+  def contact
+  end
+  def handle_form
+    puts "MESSAGE => " + request.params["name"] + " <" + request.params["email"] + ">: " + request.params["message"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get "/contact", to: "pages#contact"
+  post "/handle-form", to: "pages#handle_form"
 end
